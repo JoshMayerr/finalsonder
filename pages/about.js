bd6fd5f2d610b63462b4d256cc7681f4fee6dc71
Josh Mayer <josh.25.mayer@gmail.com>
2022-06-07 08:09:14 -0700

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,58 @@ export default function About() {
   return (
     <>
       <div className="flex flex-col font-mono items-center justify-center min-h-screen -mt-28">
-        About Sonder.
+        <h1 className="font-5xl font-extrabold">The Sonder Story.</h1>
+        <p className="font-xl text-center">
+          Coming into this term, I had a few things I knew I wanted to research
+          and incorporate into this project, but I wasn&#x27;t sure how I could
+          combine them all. Between a clothing brand, NFT research, and website
+          creation, the idea was sort of right in front of me the entire time.
+          So, with Bethany&#x27;s help, I landed on the idea for the Sonder
+          Florist Co. In 2018, as I was about to graduate middle school, I made
+          a clothing brand that essentially jumpstarted my curiosity for the
+          entire space. I took my first looks at true graphic design and website
+          creation from scratch. This brand was named, Sonder. (you can still
+          look at the website I made here:
+          https://sonderwebsite.vercel.app/home.html) Fast-forward to the
+          beginning of this term, as I knew I was about to graduate high school,
+          I wanted to return to the idea of this brand, and build on it, as
+          finally a senior. It was a weird feeling, starting from the same name,
+          but with the entirely improved skill set I was working with. I hoped
+          to move past the corny rose my 8th grade self thought was brilliant.
+          In the first few weeks of the term, I started with the overall concept
+          of the v2 brand identity. Knowing that I wanted to insert this brand
+          into the web3 space, which is notoriously filled with apes and just
+          internet inspired trash, I knew I had to tread a fine line between
+          that and the realistic fashion world. Therefore, I was most inspired
+          by minimalist, higher-end brands like Nike ACG, Billionaire Boys Club,
+          and CDG. After my research, I realized that my goal was to create a
+          brand that would be a breath of fresh air for the web3/NFT space that
+          would also bring a creative edge to the fashion space. This is mainly
+          what motivated the idea of the florist co. (in addition to the
+          .florist tld being very cheap) Building off the concept, I worked on
+          the three different logos I landed with. I am actually very proud of
+          the way these turned out. Each individual one was meant to serve a
+          different purpose within the entire brand. The full type logo is sort
+          of the main, most recognizble staple of the brand, the icon logo is
+          easily applied and most scalable, and the last wordmark logo is sort
+          of an alternative look. Once I had the logos, I could start on the
+          clothing drop. I am honestly not sure why, but I chose to incorporate
+          my worst art skill into the main aspect of the designs: hand drawing.
+          In the end, I don&#x27;t think it was terrible, but it is not the
+          aspect of the project I am most happy with. Finally, once I had all my
+          design assets in place, I could finish the website. For this website,
+          I used Next.js and Vercel to host it for free, which was quite nice. I
+          wish I had more time to implement the animations with framer motion.
+          They would have made the website a lot cleaner and smoother to use.
+          Additionally, I would have loved to go way deeper into the web3 aspect
+          and actually mint my own NFTs. It was just a lot of information to get
+          into in order to have a final product. Overall, I am really proud of
+          my work this term, and I loved the idea of being able to compare my
+          8th grade design self with my self now. Sonder v1 was in a lot of ways
+          what set me down this path, and I am very thankful for all the
+          opportunities I have had to continue to grow and improve my skills in
+          this space. So, I hope you enjoy Sonder v2, the Florist Co.
+        </p>
       </div>
     </>
   );
